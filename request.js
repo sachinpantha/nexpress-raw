f704eb3f67539f6768d381a1cef0730aa61176c4
sachin <sachinpantha69@gmail.com>
2022-03-30 22:50:03 +0545

    Pushed random codes for no reason

--- request.js
+++ request.js
@@ -1,8 +1,9 @@
 const express=require('express');
-const friendsController=require('./controllers/friends.controller');
-const messagesController=require('./controllers/messages.controller');
+const friendsRouter=require('./routes/friends.router')
+const messagesRouter = require('./routes/messages.router');
 const app= express();
 const PORT=3000; 
+//Simplifying the server as far as possible in routes
 app.use((req,res,next)=>{
     const start=Date.now();
     console.log(`${req.method} ${req.url}`)
@@ -11,9 +12,8 @@ app.use((req,res,next)=>{
     console.log(delta);
 })
 app.use(express.json());
-app.post('/friends',friendsController.postFriend)
-app.get('/friends',friendsController.getFriends)
-app.get('/friends/:friendID',friendsController.getFriend)
-app.get('/messages',messagesController.getMessages);
-app.post('/messages',messagesController.postMessage)
+
+
+app.use('/friends',friendsRouter);
+app.use('/messages',messagesRouter)
 app.listen(PORT);
