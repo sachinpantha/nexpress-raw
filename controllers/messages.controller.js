3da2f8a6510d1d141277597241eef18a15725bb6
sachin <sachinpantha69@gmail.com>
2022-04-01 22:20:00 +0545

    Testing file send with postman

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,7 @@
+ const path=require('path');
  function getMessages(req,res){
-    res.send('HElllllllo');
+    // res.send('HElllllllo');
+    res.sendFile(path.join(__dirname,'..','public','doge.jpg')) //sending files
 };
  function postMessage(req,res){
     console.log('updating messages');
